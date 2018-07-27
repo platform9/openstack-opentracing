97d56ea4be6e81ba3ad46a634fd54220821a7608
rparikh <rparikh@platform9.net>
2018-07-26 22:14:45 -0700

    Fixed the version of opentracing, opentracing_instrumentation & jaeger_client
    Using PBR to push the version information into the python package. To
    start another build use tags with a pattern "v0.1.30" onwards.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python2
 
 from setuptools import setup
-
+import os
 setup(
     name = 'openstack_opentracing',
-    version = '0.0.1',
     packages = ['openstack_opentracing', 'openstack_opentracing/test'],
+    setup_requires=['pbr'],
     install_requires = [
-        'jaeger_client>=3.10.0',
-        'opentracing_instrumentation>=2.3.0',
+        'jaeger_client==3.10.0',
+        'opentracing_instrumentation==2.4.1',
+        'opentracing==1.3.0',
         'oslo.middleware>=3.0.0',
         'oslo.config>=3.14.0',
         'oslo.service>=1.10.0',
@@ -17,5 +18,6 @@ setup(
         'requests>=2.10.0'
         ],
     scripts = [],
-    package_data = {}
+    package_data = {},
+    pbr=True
 )
