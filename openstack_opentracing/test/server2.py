01d87a3bd4601cd05b4139ca596f2d3cfba47277
rparikh <rparikh@platform9.net>
2018-08-29 16:42:44 -0700

    Fixing a typo in the remote sampler

--- openstack_opentracing/test/server2.py
+++ openstack_opentracing/test/server2.py
@@ -8,7 +8,7 @@ def start_server2():
     CONF = cfg.CONF
     print "Starting Server 2"
     server2 = start_server(CONF, '/Users/rparikh/work/misc/openstack-opentracing/openstack_opentracing/test/test-paste-2.ini', 2327)
-    time.sleep(200)
+    time.sleep(2000)
 
 if __name__ == "__main__":
     start_server2()
--- openstack_opentracing/test/server3.py
+++ openstack_opentracing/test/server3.py
@@ -8,7 +8,7 @@ def start_server3():
     CONF = cfg.CONF
     print "Starting Server 3"
     server3 = start_server(CONF, '/Users/rparikh/work/misc/openstack-opentracing/openstack_opentracing/test/test-paste-3.ini', 2328)
-    time.sleep(200)
+    time.sleep(2000)
 
 if __name__ == "__main__":
     start_server3()
